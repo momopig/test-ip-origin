ff6ac28e5ec744f99afbaa3c0577110190993683
momopig <121978614@qq.com>
2025-04-28 22:32:09 +0800

    chore

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // http://localhost:5001/test  =>  http://localhost:5001/nest-api/test
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
